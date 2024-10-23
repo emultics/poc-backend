809fc871e2695038adf828b7ecb2fa56a045f852
sumanmanna134 <manna.suman134@gmail.com>
2024-10-23 23:21:13 +0530

    avoid unused import

--- src/main/java/com/demo/poc/pra/model/CreateRequest.java
+++ src/main/java/com/demo/poc/pra/model/CreateRequest.java
@@ -1,4 +1,12 @@
 package com.demo.poc.pra.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateRequest {
     private String launch_date;
 
