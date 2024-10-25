e7125b8997b59cbe3e36e2dda9e02ed9087a7ef7
sumanmanna134 <manna.suman134@gmail.com>
2024-10-25 15:35:44 +0530

    add validation

--- src/main/java/com/pra/demo/utils/EqualityChecker.java
+++ src/main/java/com/pra/demo/utils/EqualityChecker.java
@@ -1,4 +1,56 @@
 package com.pra.demo.utils;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 public class EqualityChecker {
+    public static <T> boolean isEqual(T firstValue, T b){
+        if(firstValue == null || b == null){
+            return Objects.equals(firstValue, b);
+
+        }
+
+        if(firstValue instanceof String && b instanceof String){
+            return ((String) firstValue).equalsIgnoreCase(((String) b));
+        }
+
+        if(firstValue instanceof List && b instanceof List){
+            return isEqualLists((List<?>) firstValue, (List<?>) b);
+
+        }
+
+        if(firstValue instanceof Map && b instanceof Map){
+            return isEqualMap((Map<?, ?>) firstValue,(Map<?, ?>) b);
+        }
+
+        return firstValue.equals(b);
+    }
+
+    private static boolean isEqualLists(List<?> a, List<?> b){
+        if(a.size() != b.size()){
+            return false;
+        }
+
+        for(int i=0;i<a.size();i++){
+            if(!isEqual(a.get(i), b.get(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isEqualMap(Map<?,?> a, Map<?,?> b){
+        if(a.size() != b.size()){
+            return false;
+        }
+
+        for(Map.Entry<?,?> entry: a.entrySet()){
+            Object key = entry.getKey();
+            if(!b.containsKey(key) || !isEqual(entry.getValue(), b.get(key))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
